fd46aa0d8ac438929f1df8ac0930edb3e7461f8e
Nicolas Lanatta <nlanatta@nlanatta-ThinkPad-T440p>
2017-10-27 17:07:43 -0300

    Latest changes

--- Wheather/src/main/java/com/ml/TimerTaskImp.java
+++ Wheather/src/main/java/com/ml/TimerTaskImp.java
@@ -2,6 +2,8 @@ package com.ml;
 
 import java.util.TimerTask;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 public class TimerTaskImp extends TimerTask {
 
 	private int count;
@@ -44,26 +46,26 @@ public class TimerTaskImp extends TimerTask {
 //		System.out.println("secondone :"+secondone);
 //		System.out.println("thirthdone :"+thirthdone);
 		
-//		alignChequer(count2, firstone,secondone,thirthdone,thirthdonepos);
-//		triangleChequer(count2, firstone,secondone,thirthdone,thirthdonepos);
+//		alignChequer();
+//		triangleChequer();
 		checkSameLine();
 			
 	}
 
 	private void checkSameLine() {
-		double x1 = Math.cos(firstone);
-		double y1 = Math.sin(firstone);
-		double x2 = Math.cos(secondone);
-		double y2 = Math.sin(secondone);
-		double x3 = Math.cos(thirthdone);
-		double y3 = Math.sin(thirthdone);
+		double x1 = 500*Math.cos(firstone);
+		double y1 = 500*Math.sin(firstone);
+		double x2 = 2000*Math.cos(secondone);
+		double y2 = 2000*Math.sin(secondone);
+		double x3 = 1000*Math.cos(thirthdone);
+		double y3 = 1000*Math.sin(thirthdone);
 		
 		double m = (y2-y1)/(x2-x1);
-		double a1 = y1 - m*x1;
-		double a2 = y2 - m*x2;
-		double a3 = y3 - m*x3;
+		double a1 = Math.round(y1 - m*x1);
+		double a2 = Math.round(y2 - m*x2);
+		double a3 = Math.round(y3 - m*x3);
 		
-		if( a1 == a2 && a1 == a3)
+		if( a1 == a2 && a1 == a3 && a1 != 0)
 		{
 			System.out.println("[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[PRESION y TEMP OPTIMA]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]");
 			System.out.println("////////////////////");
